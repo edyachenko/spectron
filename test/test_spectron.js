054f4b7c17e935d5a2ff54cc70acc866f705213e
Ievgen Diachenko <Ievgen_Diachenko@epam.com>
2020-09-01 12:08:41 +0300

    Added few tests for spectron

---
+++ test/test_spectron.js
@@ -0,0 +1,23 @@
+const Application = require('spectron')
+const assert = require('assert')
+let timeout = 30000
+
+
+
+describe('Array',  async function () {
+
+    it('Test1 ',  async function () {
+        console.log('begin')
+        const app = new Application.Application({
+
+            path: 'C:/Users/Ievgen_Diachenko/AppData/Local/Programs/anatha-desktop-wallet/AnathaWallet.exe'
+
+        })
+        await app.start()
+         console.log(await app.isRunning())
+        // // Check if the window is visible
+        // await app.stop()
+        console.log('end')
+
+    });
+});
